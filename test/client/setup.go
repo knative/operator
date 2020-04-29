c0b4bc866bc4bdb2f2bf55f76d432032a4aa9679
Markus Thömmes <markusthoemmes@me.com>
2020-04-29 10:03:42 +0200

    Fix malformed license headers. (#46)

--- test/client/setup.go
+++ test/client/setup.go
@@ -1,9 +1,12 @@
 /*
 Copyright 2020 The Knative Authors
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,8 +24,8 @@ import (
 	// https://github.com/kubernetes/client-go/issues/242
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
-	pkgTest "knative.dev/pkg/test"
 	"knative.dev/operator/test"
+	pkgTest "knative.dev/pkg/test"
 )
 
 // Setup creates the client objects needed in the e2e tests.

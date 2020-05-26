c8d50d978bc91f9c2c5a628b61a15bb1be25e083
Vincent Hou <shou@us.ibm.com>
2020-05-26 17:45:18 -0400

    Add the field version into knativeserving spec

--- version/version.go
+++ version/version.go
@@ -1,8 +1,8 @@
 package version
 
 var (
-	// ServingVersion is the version that will be applied to the KnativeServing resource.
-	ServingVersion = "0.14.0"
+	// ServingVersion is the default version that will be applied to the KnativeServing resource.
+	ServingVersion = "0.15.0"
 
 	// EventingVersion is the version that will be applied to the KnativeEventing resource.
 	EventingVersion = "0.14.2"

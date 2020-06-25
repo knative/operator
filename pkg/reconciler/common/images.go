eea9ba75bcec8b5fe9834a7e295ce4fa687b0828
Thomas Shafer <tshafer@google.com>
2020-06-25 14:51:21 -0700

    Refactor image replacement interface for explicit support of each type of object

--- pkg/reconciler/common/images.go
+++ pkg/reconciler/common/images.go
@@ -43,7 +43,9 @@ var (
 
 // ImageTransformer is an interface for transforming images passed to the ResourceImageTransformer
 type ImageTransformer interface {
-	HandleImage(containerName, parent string, useDefault bool) (string, bool)
+	ImageForContainer(container *corev1.Container, parentName string) (string, bool)
+	ImageForEnvVar(env *corev1.EnvVar, parentName string) (string, bool)
+	ImageForImage(image *caching.Image, parentName string) (string, bool)
 	HandleImagePullSecrets(imagePullSecrets []corev1.LocalObjectReference, log *zap.SugaredLogger) []corev1.LocalObjectReference
 }
 
@@ -54,17 +56,29 @@ type registryImageTransformer struct {
 
 var _ ImageTransformer = (*registryImageTransformer)(nil)
 
-func (rit *registryImageTransformer) HandleImage(containerName, parent string, useDefault bool) (string, bool) {
-	if image, ok := rit.registry.Override[parent+delimiter+containerName]; ok {
+func (rit *registryImageTransformer) ImageForContainer(container *corev1.Container, parentName string) (string, bool) {
+	return rit.handleImage(container.Name, parentName, true)
+}
+
+func (rit *registryImageTransformer) ImageForEnvVar(env *corev1.EnvVar, parentName string) (string, bool) {
+	return rit.handleImage(env.Name, "", false)
+}
+
+func (rit *registryImageTransformer) ImageForImage(image *caching.Image, parentName string) (string, bool) {
+	return rit.handleImage(image.Name, "", true)
+}
+
+func (rit *registryImageTransformer) handleImage(resourceName, parentName string, useDefault bool) (string, bool) {
+	if image, ok := rit.registry.Override[parentName+delimiter+resourceName]; ok {
 		return image, true
 	}
-	if image, ok := rit.registry.Override[containerName]; ok {
+	if image, ok := rit.registry.Override[resourceName]; ok {
 		return image, true
 	}
 	if !useDefault {
 		return "", false
 	}
-	return replaceName(rit.registry.Default, containerName), true
+	return replaceName(rit.registry.Default, resourceName), true
 }
 
 func replaceName(imageTemplate string, name string) string {
@@ -177,7 +191,7 @@ func updateImage(spec *corev1.PodSpec, imageTransformer ImageTransformer, log *z
 	containers := spec.Containers
 	for index := range containers {
 		container := &containers[index]
-		newImage, _ := imageTransformer.HandleImage(container.Name, name, true)
+		newImage, _ := imageTransformer.ImageForContainer(container, name)
 		if newImage != "" {
 			updateContainer(container, newImage, log)
 		}
@@ -191,7 +205,7 @@ func updateEnvVarImages(spec *corev1.PodSpec, imageTransformer ImageTransformer,
 		container := &containers[index]
 		for envIndex := range container.Env {
 			env := &container.Env[envIndex]
-			if newImage, ok := imageTransformer.HandleImage(env.Name, "", false); ok {
+			if newImage, ok := imageTransformer.ImageForEnvVar(env, container.Name); ok {
 				env.Value = newImage
 			}
 		}
@@ -221,7 +235,7 @@ func updateCachingImage(imageTransformer ImageTransformer, u *unstructured.Unstr
 
 // updateImageSpec updates the image of a with a new registry and tag
 func updateImageSpec(image *caching.Image, imageTransformer ImageTransformer, log *zap.SugaredLogger) {
-	if newImage, _ := imageTransformer.HandleImage(image.Name, "", true); newImage != "" {
+	if newImage, _ := imageTransformer.ImageForImage(image, ""); newImage != "" {
 		log.Debugf("Updating image from: %v, to: %v", image.Spec.Image, newImage)
 		image.Spec.Image = newImage
 	}

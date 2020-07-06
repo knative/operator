ccc830d5615f37ced4ecfdbbb078b1f1abc973a1
Vincent Hou <shou@us.ibm.com>
2020-07-06 15:59:30 -0400

    Update the serving manifests into 0.14.2

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// ServingVersion is the version that will be applied to the KnativeServing resource.
-	ServingVersion = "0.14.0"
+	ServingVersion = "0.14.2"
 
 	// EventingVersion is the version that will be applied to the KnativeEventing resource.
 	EventingVersion = "0.14.2"

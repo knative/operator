32789cbcbd23e9af737969af2e9e3732a906b97c
Zhimin Xiang <zhiminx@google.com>
2020-09-23 17:18:51 +0000

    do not wait for job to be ready for applying CRD

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -139,10 +139,10 @@ function install_istio() {
   echo ">> Bringing up Istio"
   echo ">> Running Istio CRD installer"
   kubectl apply -f "${INSTALL_ISTIO_CRD_YAML}" || return 1
-  wait_until_batch_job_complete istio-system || return 1
 
   echo ">> Running Istio"
   kubectl apply -f "${INSTALL_ISTIO_YAML}" || return 1
+  wait_until_batch_job_complete istio-system || return 1
 }
 
 function create_namespace() {

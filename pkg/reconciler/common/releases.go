0bcae4ff637f3c2db37d3597428c9144536eade0
Vincent Hou <shou@us.ibm.com>
2020-10-26 10:08:39 -0400

    Refactor teh order of the fuction IsVersionValidMigrationEligible

--- pkg/reconciler/common/releases.go
+++ pkg/reconciler/common/releases.go
@@ -77,7 +77,7 @@ func InstalledManifest(instance v1alpha1.KComponent) (mf.Manifest, error) {
 // IsVersionValidMigrationEligible returns the bool indicate whether the target version is valid and the installed
 // manifest is able to upgrade or downgrade to the target manifest.
 func IsVersionValidMigrationEligible(instance v1alpha1.KComponent) error {
-	var errMess error
+	var err error
 	target := sanitizeSemver(TargetVersion(instance))
 	if !semver.IsValid(target) {
 		return fmt.Errorf("target version %v is not in a valid semantic versioning format.", target)
@@ -87,40 +87,39 @@ func IsVersionValidMigrationEligible(instance v1alpha1.KComponent) error {
 		return fmt.Errorf("target version %v should at least include the major and minor numbers.", target)
 	}
 
-	targetMinor, err := strconv.Atoi(strings.Split(target, ".")[1])
-	if err != nil {
-		return fmt.Errorf("minor number of the target version %v should be an integer.", target)
-	}
-
 	current := instance.GetStatus().GetVersion()
-	// If there is no manifest installed, return true, because the target manifest is able to install.
+	// If there is no manifest installed, return nil, because the target manifest is able to install.
 	if current == "" {
 		return nil
 	}
 
 	current = sanitizeSemver(current)
-	errMess = fmt.Errorf("not supported to upgrade or downgrade across multiple MINOR versions. The "+
-		"installed KnativeServing version is %v.", current)
 	currentMajor := semver.Major(current)
 	targetMajor := semver.Major(target)
 	if currentMajor != targetMajor {
 		// All the official releases of Knative are under the same Major version number. If target and current versions
 		// are different in terms of major version, upgrade or downgrade is not supported.
 		// TODO We need to deal with the the case of bumping major version later.
-		return errMess
+		return fmt.Errorf("not supported to upgrade or downgrade across the MAJOR version. The "+
+			"installed KnativeServing version is %v.", current)
 	}
 
 	currentMinor, err := strconv.Atoi(strings.Split(current, ".")[1])
 	if err != nil {
 		return fmt.Errorf("minor number of the current version %v should be an integer.", current)
 	}
+	targetMinor, err := strconv.Atoi(strings.Split(target, ".")[1])
+	if err != nil {
+		return fmt.Errorf("minor number of the target version %v should be an integer.", target)
+	}
 
-	// If the diff between minor versions are less than 2, return true.
+	// If the diff between minor versions are less than 2, return nil.
 	if abs(currentMinor-targetMinor) < 2 {
 		return nil
 	}
 
-	return errMess
+	return fmt.Errorf("not supported to upgrade or downgrade across multiple MINOR versions. The "+
+		"installed KnativeServing version is %v.", current)
 }
 
 func getVersionKey(instance v1alpha1.KComponent) string {

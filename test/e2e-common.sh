c2aecba6a74ed9a1ed6a007542b8c06e917be697
Vincent Hou <shou@us.ibm.com>
2021-04-06 10:59:31 -0400

    Update the previous installed versions in prow

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -18,7 +18,7 @@
 source "$(dirname "${BASH_SOURCE[0]}")/../vendor/knative.dev/hack/e2e-tests.sh"
 
 # The previous serving release, installed by the operator. This value should be in the semantic format of major.minor.
-readonly PREVIOUS_SERVING_RELEASE_VERSION="0.21"
+readonly PREVIOUS_SERVING_RELEASE_VERSION="0.22"
 # The previous eventing release, installed by the operator. This value should be in the semantic format of major.minor.
 readonly PREVIOUS_EVENTING_RELEASE_VERSION="0.21"
 # The target serving/eventing release to upgrade, installed by the operator. It can be a release available under

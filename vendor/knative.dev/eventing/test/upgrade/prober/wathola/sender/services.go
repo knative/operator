01b0ca47a10761bdd953ad38bbd37fe79813736b
Knative Automation <automation@knative.team>
2022-03-30 13:31:46 +0000

    upgrade to latest dependencies
    bumping k8s.io/code-generator fb849a7...fb849a7:
    bumping knative.dev/caching defaf7b...0755a97:
      > 0755a97 upgrade to latest dependencies (# 617)
    bumping knative.dev/eventing 4be06cd...02663e2:
      > 02663e2 Update community files (# 6290)
      > 04eafff Process empty tracing config in test images (# 6289)
      > 3890b39 Wathola Tracing for upgrade tests (# 6219)
      > eb2bfff upgrade to latest dependencies (# 6288)
      > 7001b65 upgrade to latest dependencies (# 6286)
    bumping knative.dev/hack 14f832e...f067737:
      > f067737 Adding code of conduct to reference knative/community's CoC (# 162)
    bumping k8s.io/api a1613d7...cd271e7:
      > cd271e7 Update dependencies to v0.23.5 tag
      > a6b2207 Merge pull request # 108139 from liggitt/automated-cherry-pick-of-# 108138-upstream-release-1.23
      > 80b93d4 Revert v1beta1 PodDisruptionBudget select patchStrategy
    bumping k8s.io/apimachinery 276a8a7...276a8a7:
    bumping k8s.io/client-go b425fb6...97582f2:
      > 97582f2 Update dependencies to v0.23.5 tag
    bumping knative.dev/pkg e6e3cf0...0a1ec2e:
      > 0a1ec2e upgrade to latest dependencies (# 2474)
      > 1f7514a bump k8s deps to v0.23.5 (# 2469)
      > 19a2589 fix coverage settings (# 2471)
      > 2daa86a Electors can provide an initial set of buckets (# 2473)
    bumping knative.dev/serving ccc4040...73d4e7c:
      > 73d4e7c Update net-kourier nightly (# 12794)
      > 40be138 Update net-gateway-api nightly (# 12793)
      > f216d7b Fix racey tests by not adding things directly to the informer cache (# 12795)
      > 15f7a4e Update net-kourier nightly (# 12781)
      > 1255dc7 upgrade to latest dependencies (# 12787)
      > 6a0c9cb Update net-gateway-api nightly (# 12780)
      > ad64980 use ctx setup by the test which has a test logger (# 12784)
      > 0bc4171 Adds progressDeadline annotation on per revision basis (# 12751)
      > 082ed55 Make some of the unit tests a bit more robust (# 12783)
    
    Signed-off-by: Knative Automation <automation@knative.team>

---
+++ vendor/knative.dev/eventing/test/upgrade/prober/wathola/sender/services.go
@@ -0,0 +1,247 @@
+/*
+Copyright 2021 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sender
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	cloudeventshttp "github.com/cloudevents/sdk-go/v2/protocol/http"
+	"github.com/wavesoftware/go-ensure"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/trace"
+	"knative.dev/eventing/test/upgrade/prober/wathola/config"
+	"knative.dev/eventing/test/upgrade/prober/wathola/event"
+	"knative.dev/pkg/tracing/propagation/tracecontextb3"
+)
+
+const (
+	Name = "wathola-sender"
+)
+
+var (
+	// ErrEndpointTypeNotSupported is raised if configured endpoint isn't
+	// supported by any of the event senders that are registered.
+	ErrEndpointTypeNotSupported = errors.New("given endpoint isn't " +
+		"supported by any registered event sender")
+	log                     = config.Log
+	senderConfig            = &config.Instance.Sender
+	eventSenders            = make([]EventSender, 0, 1)
+	eventSendersWithContext = make([]EventSenderWithContext, 0, 1)
+)
+
+type sender struct {
+	// eventsSent is the number of events successfully sent
+	eventsSent int
+	// totalRequests is the number of all the send event requests
+	totalRequests int
+	// unavailablePeriods is an array for non-zero retries for each event
+	unavailablePeriods []time.Duration
+}
+
+func (s *sender) SendContinually() {
+	var shutdownCh = make(chan struct{})
+	defer func() {
+		s.sendFinished()
+		// Give time to send tracing information.
+		// https://github.com/knative/pkg/issues/2475
+		time.Sleep(5 * time.Second)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-c
+		// sig is a ^C or term, handle it
+		log.Infof("%v signal received, closing", sig.String())
+		close(shutdownCh)
+	}()
+
+	var start time.Time
+	retry := 0
+	for {
+		select {
+		case <-shutdownCh:
+			// If there is ongoing event transition, we need to push to retries too.
+			if retry != 0 {
+				s.unavailablePeriods = append(s.unavailablePeriods, time.Since(start))
+			}
+			return
+		default:
+		}
+		err := s.sendStep()
+		if err != nil {
+			if retry == 0 {
+				start = time.Now()
+			}
+			log.Warnf("Could not send step event %v, retrying (%d)",
+				s.eventsSent, retry)
+			log.Debug("Error: ", err)
+			retry++
+		} else {
+			if retry != 0 {
+				s.unavailablePeriods = append(s.unavailablePeriods, time.Since(start))
+				log.Warnf("Event sent after %v retries", retry)
+				retry = 0
+			}
+			time.Sleep(senderConfig.Interval)
+		}
+	}
+}
+
+// NewCloudEvent creates a new cloud event
+func NewCloudEvent(data interface{}, typ string) cloudevents.Event {
+	e := cloudevents.NewEvent()
+	e.SetDataContentType("application/json")
+	e.SetType(typ)
+	host, err := os.Hostname()
+	ensure.NoError(err)
+	e.SetSource(fmt.Sprintf("knative://%s/wathola/sender", host))
+	e.SetID(NewEventID())
+	e.SetTime(time.Now().UTC())
+	err = e.SetData(cloudevents.ApplicationJSON, data)
+	ensure.NoError(err)
+	errs := e.Validate()
+	if errs != nil {
+		ensure.NoError(errs)
+	}
+	return e
+}
+
+// ResetEventSenders will reset configured event senders to defaults.
+func ResetEventSenders() {
+	eventSenders = make([]EventSender, 0, 1)
+	eventSendersWithContext = make([]EventSenderWithContext, 0, 1)
+}
+
+// RegisterEventSender will register a EventSender to be used.
+// Deprecated. Use RegisterEventSenderWithContext.
+func RegisterEventSender(es EventSender) {
+	eventSenders = append(eventSenders, es)
+}
+
+// RegisterEventSenderWithContext will register EventSenderWithContext to be used.
+func RegisterEventSenderWithContext(es EventSenderWithContext) {
+	eventSendersWithContext = append(eventSendersWithContext, es)
+}
+
+// SendEvent will send cloud event to given url
+func SendEvent(ctx context.Context, ce cloudevents.Event, endpoint interface{}) error {
+	sendersWithCtx := make([]EventSenderWithContext, 0, len(eventSendersWithContext)+1)
+	sendersWithCtx = append(sendersWithCtx, eventSendersWithContext...)
+	if len(eventSendersWithContext) == 0 && len(eventSenders) == 0 {
+		sendersWithCtx = append(sendersWithCtx, httpSender{})
+	}
+	for _, eventSender := range sendersWithCtx {
+		if eventSender.Supports(endpoint) {
+			return eventSender.SendEventWithContext(ctx, ce, endpoint)
+		}
+	}
+	// Backwards compatibility.
+	// TODO: Remove when downstream repositories start using EventSenderWithContext.
+	senders := make([]EventSender, 0, len(eventSenders)+1)
+	senders = append(senders, eventSenders...)
+	for _, eventSender := range senders {
+		if eventSender.Supports(endpoint) {
+			return eventSender.SendEvent(ce, endpoint)
+		}
+	}
+	return fmt.Errorf("%w: endpoint is %#v", ErrEndpointTypeNotSupported, endpoint)
+}
+
+type httpSender struct{}
+
+func (h httpSender) Supports(endpoint interface{}) bool {
+	switch url := endpoint.(type) {
+	default:
+		return false
+	case string:
+		return strings.HasPrefix(url, "http://") ||
+			strings.HasPrefix(url, "https://")
+	}
+}
+
+func (h httpSender) SendEvent(ce cloudevents.Event, endpoint interface{}) error {
+	return h.SendEventWithContext(context.Background(), ce, endpoint)
+}
+
+func (h httpSender) SendEventWithContext(ctx context.Context, ce cloudevents.Event, endpoint interface{}) error {
+	url := endpoint.(string)
+	opts := []cloudeventshttp.Option{
+		cloudevents.WithRoundTripper(&ochttp.Transport{
+			Propagation: tracecontextb3.TraceContextEgress,
+		}),
+	}
+	c, err := cloudevents.NewClientHTTP(opts...)
+	if err != nil {
+		return err
+	}
+	ctxWithTarget := cloudevents.ContextWithTarget(ctx, url)
+	result := c.Send(ctxWithTarget, ce)
+	if cloudevents.IsACK(result) {
+		return nil
+	}
+	return result
+}
+
+func (s *sender) sendStep() error {
+	step := event.Step{Number: s.eventsSent + 1}
+	ce := NewCloudEvent(step, event.StepType)
+	ctx, span := PopulateSpanWithEvent(context.Background(), ce, Name)
+	defer span.End()
+	span.AddAttributes(trace.Int64Attribute("step", int64(step.Number)))
+	endpoint := senderConfig.Address
+	log.Infof("Sending step event #%v to %#v", step.Number, endpoint)
+	err := SendEvent(ctx, ce, endpoint)
+	// Record every request regardless of the result
+	s.totalRequests++
+	if err != nil {
+		return err
+	}
+	s.eventsSent++
+	return nil
+}
+
+func (s *sender) sendFinished() {
+	if s.eventsSent == 0 {
+		return
+	}
+	finished := event.Finished{EventsSent: s.eventsSent, TotalRequests: s.totalRequests, UnavailablePeriods: s.unavailablePeriods}
+	endpoint := senderConfig.Address
+	ce := NewCloudEvent(finished, event.FinishedType)
+	ctx, span := PopulateSpanWithEvent(context.Background(), ce, Name)
+	defer span.End()
+	log.Infof("Sending finished event (count: %v) to %#v", finished.EventsSent, endpoint)
+	ensure.NoError(SendEvent(ctx, ce, endpoint))
+}
+
+func PopulateSpanWithEvent(ctx context.Context, ce cloudevents.Event, spanName string) (context.Context, *trace.Span) {
+	ctxWithSpan, span := trace.StartSpan(ctx, spanName)
+	span.AddAttributes(
+		trace.StringAttribute("cloudevents.type", ce.Type()),
+		trace.StringAttribute("cloudevents.id", ce.ID()),
+	)
+	return ctxWithSpan, span
+}

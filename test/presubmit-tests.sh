8404fc5fddc97983eb1f22c4d2c89cb041380bc7
Kelly A <kellyaa@users.noreply.github.com>
2022-07-21 12:07:20 -0400

    remove defunct markdown linting variable (#1133)

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -21,9 +21,6 @@
 # Use the flags --build-tests, --unit-tests and --integration-tests
 # to run a specific set of tests.
 
-# Markdown linting failures don't show up properly in Gubernator resulting
-# in a net-negative contributor experience.
-export DISABLE_MD_LINTING=1
 export GO111MODULE=on
 
 source $(dirname $0)/../vendor/knative.dev/hack/presubmit-tests.sh

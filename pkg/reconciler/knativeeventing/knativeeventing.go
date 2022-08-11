e0d5ef67c9d3f6424ba7a993db1afe74e91ea057
Vincent <shou@us.ibm.com>
2022-08-11 16:18:05 -0400

    Tolerate the case that the existing eventing version is not found (#1160)
    Signed-off-by: Vincent Hou <shou@us.ibm.com>
    
    Signed-off-by: Vincent Hou <shou@us.ibm.com>

--- pkg/reconciler/knativeeventing/knativeeventing.go
+++ pkg/reconciler/knativeeventing/knativeeventing.go
@@ -84,7 +84,11 @@ func (r *Reconciler) FinalizeKind(ctx context.Context, original *v1beta1.Knative
 		logger.Error("Unable to fetch installed manifest; no cluster-scoped resources will be finalized", err)
 		return nil
 	}
-	return common.Uninstall(manifest)
+
+	if err = common.Uninstall(manifest); err != nil {
+		logger.Error("Failed to finalize platform resources", err)
+	}
+	return nil
 }
 
 // ReconcileKind compares the actual state with the desired, and attempts to

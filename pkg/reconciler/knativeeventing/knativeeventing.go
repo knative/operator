2ba5cc1e7b417200002513e7d017c2a01570dd3c
Vincent Hou <shou@us.ibm.com>
2022-08-12 13:52:09 -0400

    Remove the sources if they are disabled
    Signed-off-by: Vincent Hou <shou@us.ibm.com>

--- pkg/reconciler/knativeeventing/knativeeventing.go
+++ pkg/reconciler/knativeeventing/knativeeventing.go
@@ -140,7 +140,7 @@ func (r *Reconciler) transform(ctx context.Context, manifest *mf.Manifest, comp 
 func (r *Reconciler) installed(ctx context.Context, instance base.KComponent) (*mf.Manifest, error) {
 	// Create new, empty manifest with valid client and logger
 	installed := r.manifest.Append()
-	stages := common.Stages{common.AppendInstalled, source.AppendInstalledSources, r.transform}
+	stages := common.Stages{common.AppendInstalled, source.AppendAllSources, r.transform}
 	err := stages.Execute(ctx, &installed, instance)
 	return &installed, err
 }

5bf70185b9939defd4ca0c7d43c8e6dddd2284ff
Vincent Hou <shou@us.ibm.com>
2022-10-19 12:54:46 -0400

    Test 1.6 to 1.7 by using 1.7 code base

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -18,14 +18,14 @@
 source "$(dirname "${BASH_SOURCE[0]}")/../vendor/knative.dev/hack/e2e-tests.sh"
 
 # The previous serving release, installed by the operator. This value should be in the semantic format of major.minor.
-readonly PREVIOUS_SERVING_RELEASE_VERSION="1.7"
+readonly PREVIOUS_SERVING_RELEASE_VERSION="1.6"
 # The previous eventing release, installed by the operator. This value should be in the semantic format of major.minor.
-readonly PREVIOUS_EVENTING_RELEASE_VERSION="1.7"
+readonly PREVIOUS_EVENTING_RELEASE_VERSION="1.6"
 # The target serving/eventing release to upgrade, installed by the operator. It can be a release available under
 # kodata or an incoming new release. This value should be in the semantic format of major.minor.
-readonly TARGET_RELEASE_VERSION="1.8"
+readonly TARGET_RELEASE_VERSION="1.7"
 # This is the branch name of knative repos, where we run the upgrade tests.
-readonly KNATIVE_REPO_BRANCH="release-1.8"
+readonly KNATIVE_REPO_BRANCH="release-1.7"
 # Namespaces used for tests
 # This environment variable TEST_NAMESPACE defines the namespace to install Knative Serving.
 export TEST_NAMESPACE="${TEST_NAMESPACE:-knative-operator-testing}"

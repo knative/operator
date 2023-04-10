e28e5138be08fa9f8ceb497f860336dc1a668976
l-qing <qq190219044@163.com>
2023-04-11 05:32:48 +0800

    fix: does not panic when errors occur during delete (#1391)
    * fix: does not panic when errors occur during delete
    
    Ref:
    - https://github.com/manifestival/manifestival/issues/92
    
    * chore: update codegen
    
    All of these modifications were automatically generated by running commands:
    `./hack/update-codegen.sh`

--- pkg/reconciler/common/extensions_test.go
+++ pkg/reconciler/common/extensions_test.go
@@ -78,22 +78,25 @@ func TestExtensions(t *testing.T) {
 			if ext != nil {
 				transformers := ext.Transformers(nil)
 				if len(transformers) != test.length {
-					t.Error("Unexpected result")
+					t.Errorf("Unexpected transformers length. Expected %d, got %d", test.length, len(transformers))
 				}
-				if ext.Reconcile(context.TODO(), nil) != nil {
-					t.Error("Unexpected result")
+				if err := ext.Reconcile(context.TODO(), nil); err != nil {
+					t.Errorf("Extensions reconcile failed. error: %v", err)
 				}
 				if test.length == 1 {
 					manifests, err := test.platform.Manifests(nil)
 					if err != nil {
-						t.Error("Unexpected result")
+						t.Errorf("Extensions manifests failed. error: %v", err)
+					}
+					if len(manifests) == 0 {
+						t.Fatal("manifests is empty")
 					}
 					if err := Transform(context.TODO(), &manifests[0], &v1beta1.KnativeServing{}, transformers...); err != nil {
-						t.Error("Unexpected result")
+						t.Errorf("Transform failed. error: %v", err)
 					}
 					for _, r := range manifests[0].Resources() {
 						if r.GetNamespace() != string(ext.(TestExtension)) {
-							t.Error("Unexpected result")
+							t.Logf("Expected namespace: %s, got: %s", string(ext.(TestExtension)), r.GetNamespace())
 						}
 					}
 				}

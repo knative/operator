03cb4f8a667cce15bc2ad3d3c9474379732c4091
Vincent Hou <shou73@bloomberg.net>
2023-05-18 17:02:59 -0400

    Add the helm template and the instruction on how to release helm chart

--- docs/release.md
+++ docs/release.md
@@ -55,3 +55,58 @@ The process to publish a version on [OperatorHub](https://operatorhub.io/operato
 - Create a PR will all the changes above an submit to the repository `k8s-operatorhub/community-operators`.
 
 Once the PR is reviewed and merged, Knative Operator of the new version is published in [Operatorhub.io](https://operatorhub.io/operator/knative-operator).
+
+## Release the Helm chart
+
+Open the file `hack/generate-helm.sh`, set the variable `VERSION` to the version of the Knative Operator, e.g. `VERSION=1.10.1`.
+
+Open a terminal, go to the home directory of the Knative Operator repository. Run the script:
+```shell
+./hack/generate-helm.sh
+```
+
+This script will generate the templates at `charts/knative-operator/templates/operator.yaml`. There are two deployment
+resources in this template: `knative-operator` and `operator-webhook`. Make sure you add the following environment variables
+into the containers of these two deployments as below:
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: knative-operator
+  ...
+spec:
+  template:
+    spec:
+      containers:
+        - name: knative-operator
+        ...
+          env:
+            ...
+            - name: KUBERNETES_MIN_VERSION
+              value: "{{ .Values.knative-operator.kubernetes_min_version }}"
+            ...      
+```
+
+and
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: operator-webhook
+  ...
+spec:
+  template:
+    spec:
+      containers:
+        - name: operator-webhook
+        ...
+          env:
+            ...
+            - name: KUBERNETES_MIN_VERSION
+              value: "{{ .Values.knative-operator.kubernetes_min_version }}"
+            ...      
+```
+
+You 
---
+++ hack/generate-helm.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Copyright 2023 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+VERSION=1.10.1
+
+# The directory used to save the helm templates.
+readonly TARGET_DIR="charts/knative-operator"
+
+# Create the directory, if it does not exist.
+mkdir -p ${TARGET_DIR}/templates
+
+# Download the released yaml file to the target directory, based on the specified version.
+wget https://github.com/knative/operator/releases/download/knative-v${VERSION}/operator.yaml -O ${TARGET_DIR}/templates/operator.yaml
+
+# Replace the namespace and images with the heml parameters
+sed -i.bak 's/namespace: default/namespace: "{{ .Release.Namespace }}"/g' ${TARGET_DIR}/templates/operator.yaml
+sed -i.bak 's/image: gcr.io\/knative-releases\/knative.dev\/operator\/cmd\/operator.*/image: "{{ .Values.knative-operator.knative-operator.image }}:{{ .Values.knative-operator.knative-operator.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
+sed -i.bak 's/image: gcr.io\/knative-releases\/knative.dev\/operator\/cmd\/webhook.*/image: "{{ .Values.knative-operator.operator-webhook.image }}:{{ .Values.knative-operator.operator-webhook.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
+
+rm ${TARGET_DIR}/templates/operator.yaml.bak

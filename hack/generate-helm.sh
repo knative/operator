619e0155a5493c42ed780dc540334ac484c5a7af
Vincent Hou <shou73@bloomberg.net>
2023-07-20 13:52:10 -0400

    Generate the charts from the config/

--- hack/generate-helm.sh
+++ hack/generate-helm.sh
@@ -26,9 +26,7 @@ readonly TARGET_DIR="charts/knative-operator"
 # Create the directory, if it does not exist.
 mkdir -p ${TARGET_DIR}/templates
 
-# Download the released yaml file to the target directory, based on the specified version.
-#wget https://github.com/knative/operator/releases/download/knative-v${VERSION}/operator.yaml -O ${TARGET_DIR}/templates/operator.yaml
-
+# Generate the template based on the yaml files under config
 echo "" > ${TARGET_DIR}/templates/operator.yaml
 for filename in config/*.yaml; do
   cat $filename >> ${TARGET_DIR}/templates/operator.yaml
@@ -37,7 +35,9 @@ done
 
 # Replace the namespace and images with the helm parameters
 sed -i.bak 's/namespace: default/namespace: "{{ .Release.Namespace }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/image: gcr.io\/knative-releases\/knative.dev\/operator\/cmd\/operator.*/image: "{{ .Values.knative_operator.knative_operator.image }}:{{ .Values.knative_operator.knative_operator.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/image: gcr.io\/knative-releases\/knative.dev\/operator\/cmd\/webhook.*/image: "{{ .Values.knative_operator.operator_webhook.image }}:{{ .Values.knative_operator.operator_webhook.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
+sed -i.bak 's/image: ko:\/\/knative.dev\/operator\/cmd\/operator/image: "{{ .Values.knative_operator.knative_operator.image }}:{{ .Values.knative_operator.knative_operator.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
+sed -i.bak 's/image: ko:\/\/knative.dev\/operator\/cmd\/webhook/image: "{{ .Values.knative_operator.operator_webhook.image }}:{{ .Values.knative_operator.operator_webhook.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
+sed -i.bak 's/operator.knative.dev\/release: devel/operator.knative.dev\/release: "v{{ .Chart.Version }}"/g' ${TARGET_DIR}/templates/operator.yaml
+sed -i.bak 's/app.kubernetes.io\/version: devel/app.kubernetes.io\/version: "{{ .Chart.Version }}"/g' ${TARGET_DIR}/templates/operator.yaml
 
 rm ${TARGET_DIR}/templates/operator.yaml.bak

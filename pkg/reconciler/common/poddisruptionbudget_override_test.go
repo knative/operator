e72bc87d66877088a274a7f57360581c1a65ef08
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-10-02 21:19:31 +0900

    Drop autoscaling/v2beta1 and policy/v1beta1

--- pkg/reconciler/common/poddisruptionbudget_override_test.go
+++ pkg/reconciler/common/poddisruptionbudget_override_test.go
@@ -22,103 +22,12 @@ import (
 	"github.com/google/go-cmp/cmp"
 	mf "github.com/manifestival/manifestival"
 	policyv1 "k8s.io/api/policy/v1"
-	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes/scheme"
 
 	"knative.dev/operator/pkg/apis/operator/base"
 	servingv1beta1 "knative.dev/operator/pkg/apis/operator/v1beta1"
 )
-
-func TestPodDisruptionBudgetsTransformV1beta1(t *testing.T) {
-	tests := []struct {
-		name            string
-		testName        string
-		overrides       []base.PodDisruptionBudgetOverride
-		expMinAvailable *intstr.IntOrString
-	}{{
-		name:     "simple override",
-		testName: "activator-pdb",
-		overrides: []base.PodDisruptionBudgetOverride{
-			{
-				Name: "activator-pdb",
-				PodDisruptionBudgetSpec: policyv1.PodDisruptionBudgetSpec{
-					MinAvailable: &intstr.IntOrString{StrVal: "50%", Type: intstr.String},
-				},
-			},
-		},
-		expMinAvailable: &intstr.IntOrString{StrVal: "50%", Type: intstr.String},
-	}, {
-		name:            "no override",
-		overrides:       nil,
-		testName:        "activator-pdb",
-		expMinAvailable: &intstr.IntOrString{StrVal: "80%", Type: intstr.String},
-	}, {
-		name:     "override with no MinVailable",
-		testName: "activator-pdb",
-		overrides: []base.PodDisruptionBudgetOverride{
-			{
-				Name: "activator-pdb",
-				PodDisruptionBudgetSpec: policyv1.PodDisruptionBudgetSpec{
-					MinAvailable: nil,
-				},
-			},
-		},
-		expMinAvailable: &intstr.IntOrString{StrVal: "80%", Type: intstr.String},
-	}}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			manifest, err := mf.NewManifest("testdata/manifest.yaml")
-			if err != nil {
-				t.Fatalf("Failed to create manifest: %v", err)
-			}
-
-			ks := &servingv1beta1.KnativeServing{
-				Spec: servingv1beta1.KnativeServingSpec{
-					CommonSpec: base.CommonSpec{
-						PodDisruptionBudgetOverride: test.overrides,
-					},
-				},
-			}
-
-			manifest, err = manifest.Transform(PodDisruptionBudgetsTransform(ks, log))
-			if err != nil {
-				t.Fatalf("Failed to transform manifest: %v", err)
-			}
-
-			if test.overrides == nil {
-				for _, u := range manifest.Resources() {
-					if u.GetKind() == "PodDisruptionBudget" && u.GetName() == test.testName {
-						got := &policyv1beta1.PodDisruptionBudget{}
-						if err := scheme.Scheme.Convert(&u, got, nil); err != nil {
-							t.Fatalf("Failed to convert unstructured to PodDisruptionBudget: %v", err)
-						}
-
-						if diff := cmp.Diff(*got.Spec.MinAvailable, *test.expMinAvailable); diff != "" {
-							t.Fatalf("Unexpected minAvailable: %v", diff)
-						}
-					}
-				}
-			}
-
-			for _, override := range test.overrides {
-				for _, u := range manifest.Resources() {
-					if u.GetKind() == "PodDisruptionBudget" && u.GetName() == override.Name {
-						got := &policyv1beta1.PodDisruptionBudget{}
-						if err := scheme.Scheme.Convert(&u, got, nil); err != nil {
-							t.Fatalf("Failed to convert unstructured to PodDisruptionBudget: %v", err)
-						}
-
-						if diff := cmp.Diff(*got.Spec.MinAvailable, *test.expMinAvailable); diff != "" {
-							t.Fatalf("Unexpected minAvailable: %v", diff)
-						}
-					}
-				}
-			}
-		})
-	}
-}
 
 func TestPodDisruptionBudgetsTransformV1(t *testing.T) {
 	tests := []struct {

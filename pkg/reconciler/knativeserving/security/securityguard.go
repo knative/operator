2c2629667d0cd07bcd94a098cbe80d51bd6a836b
Vincent Hou <shou73@bloomberg.net>
2024-01-24 13:45:55 -0500

    Update the deps and the manifests

--- pkg/reconciler/knativeserving/security/securityguard.go
+++ pkg/reconciler/knativeserving/security/securityguard.go
@@ -33,10 +33,10 @@ import (
 var (
 	// SecurityGuardVersion is the hash map to maintain the relationship between knative version and the security guard version
 	SecurityGuardVersion = map[string]string{
+		"v1.13": "0.6",
 		"v1.12": "0.6",
 		"v1.11": "0.6",
 		"v1.10": "0.6",
-		"v1.9":  "0.6",
 	}
 
 	// QueueProxyMountPodInfoKey is the key for the QueueProxyMountPodInfo

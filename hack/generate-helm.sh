97efcabf2a2972799e43718128c9c85dd42fa2c3
Vincent <shou73@bloomberg.net>
2024-05-21 21:56:51 -0400

    Removed the namespace in Helm template (#1805)

--- hack/generate-helm.sh
+++ hack/generate-helm.sh
@@ -39,11 +39,15 @@ mkdir -p ${TARGET_DIR}/templates
 # Generate the template based on the yaml files under config
 echo "" > ${TARGET_DIR}/templates/operator.yaml
 for filename in config/*.yaml; do
+  if [[ $filename == *namespace.yaml ]]; then
+      continue
+  fi
   cat $filename >> ${TARGET_DIR}/templates/operator.yaml
   echo -e "\n---" >> ${TARGET_DIR}/templates/operator.yaml
 done
 
 # Replace the namespace and images with the helm parameters
+sed -i.bak 's/namespace: knative-operator/namespace: "{{ .Release.Namespace }}"/g' ${TARGET_DIR}/templates/operator.yaml
 sed -i.bak 's/image: ko:\/\/knative.dev\/operator\/cmd\/operator/image: "{{ .Values.knative_operator.knative_operator.image }}:{{ .Values.knative_operator.knative_operator.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
 sed -i.bak 's/image: ko:\/\/knative.dev\/operator\/cmd\/webhook/image: "{{ .Values.knative_operator.operator_webhook.image }}:{{ .Values.knative_operator.operator_webhook.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
 sed -i.bak 's/operator.knative.dev\/release: devel/operator.knative.dev\/release: "v{{ .Chart.Version }}"/g' ${TARGET_DIR}/templates/operator.yaml

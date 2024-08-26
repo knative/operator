72cfb93cfafa031f2784941e9eef04658055f0c8
Vincent <shou73@bloomberg.net>
2024-08-26 15:02:05 -0400

    [release-1.15]: Configure the securityContext, resources and pod annotations (#1877)
    * Configure the securityContext, resources and pod annotations
    
    * Updated the manifests for 1.15
    
    * Change generate helm script
    
    * Fixed the podAnnotations issues

--- hack/generate-helm.sh
+++ hack/generate-helm.sh
@@ -36,26 +36,6 @@ readonly TARGET_DIR="${CHARTS_DIR}/${NAME}"
 # Create the directory, if it does not exist.
 mkdir -p ${TARGET_DIR}/templates
 
-# Generate the template based on the yaml files under config
-echo "" > ${TARGET_DIR}/templates/operator.yaml
-for filename in config/*.yaml; do
-  if [[ $filename == *namespace.yaml ]]; then
-      continue
-  fi
-  cat $filename >> ${TARGET_DIR}/templates/operator.yaml
-  echo -e "\n---" >> ${TARGET_DIR}/templates/operator.yaml
-done
-
-# Replace the namespace and images with the helm parameters
-sed -i.bak 's/namespace: knative-operator/namespace: "{{ .Release.Namespace }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/image: ko:\/\/knative.dev\/operator\/cmd\/operator/image: "{{ .Values.knative_operator.knative_operator.image }}:{{ .Values.knative_operator.knative_operator.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/image: ko:\/\/knative.dev\/operator\/cmd\/webhook/image: "{{ .Values.knative_operator.operator_webhook.image }}:{{ .Values.knative_operator.operator_webhook.tag }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/operator.knative.dev\/release: devel/operator.knative.dev\/release: "v{{ .Chart.Version }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/app.kubernetes.io\/version: devel/app.kubernetes.io\/version: "{{ .Chart.Version }}"/g' ${TARGET_DIR}/templates/operator.yaml
-sed -i.bak 's/value: ""/value: "{{ .Values.knative_operator.kubernetes_min_version }}"/g' ${TARGET_DIR}/templates/operator.yaml
-
-rm ${TARGET_DIR}/templates/operator.yaml.bak
-
 sed -i.bak "s/{{ version }}/${VERSION:1}/g" ${TARGET_DIR}/Chart.yaml
 sed -i.bak "s/{{ tag }}/${VERSION}/g" ${TARGET_DIR}/values.yaml
 

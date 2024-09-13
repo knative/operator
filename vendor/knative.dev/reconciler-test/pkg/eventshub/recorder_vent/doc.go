1f81c6fb6924cb393da4084d71b55bde075fab9e
Martin Gencur <mgencur@redhat.com>
2024-09-13 12:12:54 +0200

    Vendor eventshub and update dependencies

--- vendor/knative.dev/reconciler-test/pkg/eventshub/recorder_vent/doc.go
+++ vendor/knative.dev/reconciler-test/pkg/eventshub/recorder_vent/doc.go
@@ -1,12 +1,12 @@
-//go:build tools
-// +build tools
-
 /*
 Copyright 2020 The Knative Authors
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,13 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tools
-
-// This package imports things required by this repository, to force `go mod` to see them as dependencies
-import (
-	_ "k8s.io/code-generator"
-	_ "knative.dev/hack"
-	_ "knative.dev/pkg/apiextensions/storageversion/cmd/migrate"
-	_ "knative.dev/pkg/hack"
-	_ "knative.dev/reconciler-test/cmd/eventshub"
-)
+// Package recorder_vent implements an eventshub.EventLog backed by Kubernetes
+// Events using an event recorder.
+package recorder_vent

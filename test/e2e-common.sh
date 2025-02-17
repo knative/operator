b2bd1e343e22995cbad5bebef08e002b28977b71
Vincent Hou <shou73@bloomberg.net>
2025-02-17 16:44:31 -0500

    [release-1.16] Upgrade the Kubernetes version in CI

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -42,7 +42,7 @@ export INGRESS_CLASS=${INGRESS_CLASS:-istio.ingress.networking.knative.dev}
 export TIMEOUT_CI=30m
 
 # GKE cluster version
-readonly K8S_CLUSTER_VERSION=1.28
+readonly K8S_CLUSTER_VERSION=1.30
 
 # Boolean used to indicate whether to generate serving YAML based on the latest code in the branch KNATIVE_SERVING_REPO_BRANCH.
 GENERATE_SERVING_YAML=0

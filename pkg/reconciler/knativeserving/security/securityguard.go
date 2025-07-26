afcf5c2b00cb88368b6e3ff85db6d1ae42dcda42
Vincent <shou73@bloomberg.net>
2025-07-26 23:31:11 +0800

    Added 1.19 kafka eventing source (#2117)
    * Added 1.19 kafka eventing source
    
    * Updated the Security Guard for 1.19

--- pkg/reconciler/knativeserving/security/securityguard.go
+++ pkg/reconciler/knativeserving/security/securityguard.go
@@ -33,10 +33,10 @@ import (
 var (
 	// SecurityGuardVersion is the hash map to maintain the relationship between knative version and the security guard version
 	SecurityGuardVersion = map[string]string{
+		"v1.19": "0.6",
 		"v1.18": "0.6",
 		"v1.17": "0.6",
 		"v1.16": "0.6",
-		"v1.15": "0.6",
 	}
 
 	// QueueProxyMountPodInfoKey is the key for the QueueProxyMountPodInfo

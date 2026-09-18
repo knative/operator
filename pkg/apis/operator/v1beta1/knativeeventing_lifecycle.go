f7462ab2462d0d749239aba8a7d09952c91b3f5a
David Simansky <dsimansk@redhat.com>
2026-09-18 14:14:41 +0200

    Fix non-contant format string err in eventing

--- pkg/apis/operator/v1beta1/knativeeventing_lifecycle.go
+++ pkg/apis/operator/v1beta1/knativeeventing_lifecycle.go
@@ -133,7 +133,7 @@ func (es *KnativeEventingStatus) MarkTargetClusterResolved() {
 
 // MarkTargetClusterNotResolved marks the TargetClusterResolved status as false with the given reason and message.
 func (es *KnativeEventingStatus) MarkTargetClusterNotResolved(reason, msg string) {
-	eventingCondSet.Manage(es).MarkFalse(base.TargetClusterResolved, reason, msg)
+	eventingCondSet.Manage(es).MarkFalse(base.TargetClusterResolved, reason, "%s", msg)
 }
 
 // GetVersion gets the currently installed version of the component.
